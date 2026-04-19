42575300d2a8ac5ec4a468fa954defe66212f610
miaizhe <ftyfty2021@163.com>
2026-04-19 21:10:59 +0800

    fix hook

--- fs/read_write.c
+++ fs/read_write.c
@@ -25,16 +25,6 @@
 #include <linux/uaccess.h>
 #include <asm/unistd.h>
 
-#ifdef CONFIG_KSU
-extern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr, 
-                               size_t *count_ptr, loff_t **pos_ptr);
-extern int ksu_handle_vfs_write(struct file **file_ptr, const char __user **buf_ptr, 
-                                size_t *count_ptr, loff_t **pos_ptr);
-#else
-static inline int ksu_handle_vfs_read(struct file **f, char __user **b, size_t *c, loff_t **p) { return 0; }
-static inline int ksu_handle_vfs_write(struct file **f, const char __user **b, size_t *c, loff_t **p) { return 0; }
-#endif
-
 const struct file_operations generic_ro_fops = {
 	.llseek		= generic_file_llseek,
 	.read_iter	= generic_file_read_iter,
@@ -453,20 +443,9 @@ ssize_t kernel_read(struct file *file, void *buf, size_t count, loff_t *pos)
 }
 EXPORT_SYMBOL_NS(kernel_read, ANDROID_GKI_VFS_EXPORT_ONLY);
 
-#ifdef CONFIG_KSU
-extern bool ksu_vfs_read_hook __read_mostly;
-extern int ksu_handle_vfs_read(struct file **file_ptr, char __user **buf_ptr,
-			size_t *count_ptr, loff_t **pos);
-#endif
-
 ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
 	ssize_t ret;
-	
-   #ifdef CONFIG_KSU 
-	if (unlikely(ksu_vfs_read_hook))
-		ksu_handle_vfs_read(&file, &buf, &count, &pos);
-   #endif
 
 	if (!(file->f_mode & FMODE_READ))
 		return -EBADF;
@@ -479,7 +458,6 @@ ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 	if (!ret) {
 		if (count > MAX_RW_COUNT)
 			count =  MAX_RW_COUNT;
-			ksu_handle_vfs_read(&file, &buf, &count, &pos);
 		ret = __vfs_read(file, buf, count, pos);
 		if (ret > 0) {
 			fsnotify_access(file);
